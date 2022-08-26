2f13685e067b226031a976a9ae3c684045fa9956
CHepworth9 <chepworth@sudscreative.com>
2022-08-26 16:13:09 -0600

    Sides of Dice

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function myfunction(){
-    var diceRoll = parseInt(Math.random() * (7 - 1) + 1);
+    var diceSides = document.getElementById("diceSideInput").value;
+    var diceRoll = parseInt(Math.random() * (diceSides) + 1);
     document.getElementById("die1").innerHTML = diceRoll;
+    
 }
