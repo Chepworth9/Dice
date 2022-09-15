978942c9bd453a4de99e42cc49bda37c8beccd51
CHepworth9 <chepworth@sudscreative.com>
2022-09-15 16:36:58 -0600

    added a for loop with a pouch. Also killed children

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function myfunction(){
+    var dicePouch = document.getElementById("dicePouch");
+    removeAllChildNodes(dicePouch)
     var diceSides = document.getElementById("diceSideInput").value;
-    var diceRoll = parseInt(Math.random() * (diceSides) + 1);
-    document.getElementById("die1").innerHTML = diceRoll;
-    
+    var numberDice = document.getElementById("diceNumberInput").value;
+    for(var i = 0; i < numberDice; i++){
+     var die = document.createElement("div");
+     die.id = "die" + i;
+     die.className = "dice";
+     var diceRoll = parseInt(Math.random() * (diceSides) + 1);
+     die.innerHTML = diceRoll;
+     dicePouch.appendChild(die);
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
